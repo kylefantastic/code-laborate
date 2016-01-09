9a3e8cf36af37ff6986811e05117033f5255777c
Kim Allen & Mark Janzer <pair+kimallen.markjanzer@devbootcamp.com>
2016-01-09 12:27:09 -0800

    Add databasecleaner to rails_helper, Pass user signup for developer

--- spec/support/login_helpers.rb
+++ spec/support/login_helpers.rb
@@ -1,16 +1,14 @@
 module LoginHelpers
 
   def user_login
-    User.create(:email => 'user@user.com', :password => 'password')
+    User.create!(first_name: 'New', last_name: 'Person', email: 'user@user.com', password: 'password')
     visit '/users/sign_in'
     within(".new_user") do
-      fill_in 'first_name', :with => 'Platypus'
-      fill_in 'last_name', :with =>'Maximus'
       fill_in 'Email', :with => 'user@user.com'
       fill_in 'Password', :with => 'password'
     end
     click_button 'Log in'
-    click_button 'Home'
+
   end
 
 end
