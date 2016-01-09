2e5c425028d9e84875ac9c84a06e5d0028358dfa
Kim Allen & Kyle Smith <pair+kimallen.kylefantastic@devbootcamp.com>
2016-01-09 11:16:20 -0800

    Fix Form Submission

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,18 +3,18 @@
 //= require turbolinks
 //= require_tree .
 
- $(document).ready(function() {
- $('#devform').hide();
-  $('#dev').click(function() {
-   $('#devform').show();
-   $('#orgform').hide();
-    })
- $('#orgform').hide();
-  $('#org').click(function() {
-   $('#orgform').show();
-   $('#devform').hide();
-    })
- });
+ // $(document).ready(function() {
+ // $('#devform').hide();
+ //  $('#dev').click(function() {
+ //   $('#devform').show();
+ //   $('#orgform').hide();
+ //    })
+ // $('#orgform').hide();
+ //  $('#org').click(function() {
+ //   $('#orgform').show();
+ //   $('#devform').hide();
+ //    })
+ // });
 
 
 
