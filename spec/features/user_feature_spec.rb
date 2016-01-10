ab239eead2ff157830989c017975b6b4a504aa81
Danielle Cameron & Kim Allen <pair+danyell85.kimallen@devbootcamp.com>
2016-01-10 15:48:45 -0800

    Add partial test for deleting

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -76,6 +76,19 @@ describe 'feature testing', :type => :feature, js: true do
       expect(page).to have_content('Rooted in Community')
     end
   end
+  feature 'user delete' do
+    scenario 'can delete their account and by deleting organization' do
+      pending
+      #finish test after merging
+      org_user_login
+      visit(new_organization_path)
+      fill_in 'Organization Name', :with => 'Rooted in Community'
+      fill_in 'Website', :with => 'www.rootedincommunity.org'
+      fill_in 'Description', :with => 'cool@user.com'
+      click_button 'Save Organization'
+    end
+  end
+
 end
 
 #closes feature testing
