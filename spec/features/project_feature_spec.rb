aeba0d347e4f457ef7f1229483c1ec1c5c00cf68
Danielle Cameron & Kim Allen <pair+danyell85.kimallen@devbootcamp.com>
2016-01-10 12:57:21 -0800

    Add helper for fill projects, delete feature test for projects

--- spec/features/project_feature_spec.rb
+++ spec/features/project_feature_spec.rb
@@ -19,17 +19,7 @@ describe 'Developer feature testing:', :type => :feature, js: true do
     end
 
     scenario 'a user can enter and submit project' do
-      visit(new_project_path)
-      fill_in 'Title', :with => 'Toolshed'
-      fill_in 'Vision', :with => 'Share Knowledge'
-      fill_in 'Need', :with => 'People'
-      fill_in 'Description', :with => 'We are looking for someone to create a lesson and resource-sharing website'
-      fill_in 'Benefit', :with => 'Educated People'
-      fill_in 'Contact name', :with => 'Kim'
-      fill_in 'Contact email', :with => 'dude@dude.com'
-      fill_in 'Contact phone', :with => '555-555-5555'
-      fill_in 'Deadline', :with => '12-21-2016'
-      click_button 'Save Project'
+      add_project
       expect(page).to have_content('Project Proposal')
     end
   end
@@ -38,19 +28,15 @@ describe 'Developer feature testing:', :type => :feature, js: true do
       org_user_login
       @organization = Organization.create(name:"Fishy", website_url:"www.google.com", description:"I like saving fish")
     end
+    scenario 'a user can delete a project' do
+      add_project
+      click_button 'Delete Project'
+      # expect{li}.to change{li.count}.by(-1)
+      expect(page).to have_selector('li', count:0)
+    end
     scenario 'a user can click the edit projects button' do
-      visit(new_project_path)
-      fill_in 'Title', :with => 'Toolshed'
-      fill_in 'Vision', :with => 'Share Knowledge'
-      fill_in 'Need', :with => 'People'
-      fill_in 'Description', :with => 'We are looking for someone to create a lesson and resource-sharing website'
-      fill_in 'Benefit', :with => 'Educated People'
-      fill_in 'Contact name', :with => 'Kim'
-      fill_in 'Contact email', :with => 'dude@dude.com'
-      fill_in 'Contact phone', :with => '555-555-5555'
-      fill_in 'Deadline', :with => '12-21-2016'
-      sleep(2)
-      click_button 'Save Project'
+      pending
+      add_project
       click_button 'Update Project'
       expect(page).to have_button("Commit Changes")
     end
