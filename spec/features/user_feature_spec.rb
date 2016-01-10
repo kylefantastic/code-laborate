f1b9c6d0bd4d2e843bfe26fc0f1f0edf890d0a9d
Kyle Smith & Mark Janzer <pair+kylefantastic.markjanzer@devbootcamp.com>
2016-01-09 19:01:35 -0800

    Add projects page. Add test for projects index

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -23,22 +23,6 @@ describe 'feature testing', :type => :feature, js: true do
   end
 
   feature 'registration' do
-    context 'as a developer' do
-      scenario 'a user can sign up' do
-        visit(root_path)
-        click_link ('Sign up')
-        click_button('Sign Up As Developer')
-        fill_in 'First name', :with => 'Kim'
-        fill_in 'Last name', :with => 'Doe'
-        fill_in 'Email', :with => 'user@user.com'
-        fill_in 'Password', :with => 'password'
-        fill_in 'Password confirmation', :with => 'password'
-        click_button 'Sign up'
-
-        expect(page).to have_content('Logged in as user@user.com')
-      end
-    end
-
     context 'as an organization representative' do
       scenario 'a user can sign up' do
         visit(root_path)
@@ -58,7 +42,6 @@ describe 'feature testing', :type => :feature, js: true do
   feature 'login' do
     scenario 'a registered user can log in' do
       org_user_login
-user_feature_spec.rb
       expect(page).to have_link('Logout')
     end
   end
@@ -69,14 +52,14 @@ user_feature_spec.rb
       visit(organizations_new_path)
       expect(page).to have_selector('#new_org_form')
     end
-  end
+
     scenario 'a user can fill out form and see the organization profile' do
       org_user_login
       visit(organizations_new_path)
       fill_in 'Organization Name', :with => 'Rooted in Community'
       fill_in 'Website', :with => 'www.rootedincommunity.org'
       fill_in 'Description', :with => 'cool@user.com'
-
     end
+  end
 end
 #closes feature testing
