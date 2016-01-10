e802b1953208a1b0a51021dbaf58bbc878545f7b
Mark Janzer <email": null>
2016-01-10 12:20:34 -0800

    Add basic ajax for users/edit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,16 +3,44 @@
 //= require turbolinks
 //= require_tree .
 
- $(document).ready(function() {
+$(document).ready(function() {
+  registerForm()
+  renderEditUserForm()
+  editUserInfo()
+});
+
+
+function registerForm(){
   $('#dev').click(function() {
-   $('#devform').show();
-   $('#orgform').hide();
-    })
+    $('#devform').show();
+    $('#orgform').hide();
+  })
   $('#org').click(function() {
-   $('#orgform').show();
-   $('#devform').hide();
+    $('#orgform').show();
+    $('#devform').hide();
+  })
+}
+
+function renderEditUserForm(){
+  $('div').on("click", ".render-edit-user-form", function(event){
+    event.preventDefault()
+    $('.developer-profile').hide()
+    $('.edit-developer-from').show()
+  })
+}
+
+function editUserInfo(){
+  $('div').on("click", ".edit-profile", function(event){
+    event.preventDefault()
+    var userInfo = $(".edit-profile-form").serialize()
+    var request = $.ajax({
+      url: "/users/:id",
+      type: "PUT",
+      data: userInfo
     })
- });
-
-
-
+    request.done(function(response){
+      console.log(response)
+      console.log(typeof response)
+    })
+  })
+}
