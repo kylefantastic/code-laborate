b00f4d98ed0d4efe69390bcd6b1311975a37b2e8
Danielle Cameron & Kim Allen <pair+danyell85.kimallen@devbootcamp.com>
2016-01-11 15:22:15 -0800

    Add logic for redirects based on sign up or sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :email, :password, :last_name, :public_profile_url, :bootcamp, :org_affiliate) }
   end
+
+  def after_sign_up_path_for(resource)
+    new_organization_path(current_user)
+  end
 end
