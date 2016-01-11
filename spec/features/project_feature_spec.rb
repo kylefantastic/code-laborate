2d630ec0e5af8f2cdabda649c93fc020c056cfec
Danielle Cameron & Kim Allen <pair+danyell85.kimallen@devbootcamp.com>
2016-01-10 17:32:04 -0800

    Add feature testing for product error message

--- spec/features/project_feature_spec.rb
+++ spec/features/project_feature_spec.rb
@@ -7,6 +7,7 @@ end
 
 describe 'Project feature testing:', :type => :feature, js: true do
   include LoginHelpers
+  include FillInHelpers
 
   feature 'create project' do
     before :each do
@@ -22,17 +23,33 @@ describe 'Project feature testing:', :type => :feature, js: true do
       add_project
       expect(page).to have_content('Project Proposal')
     end
+
+    scenario 'if a user doesnt fill out all forms it will show 8 error messages' do
+      visit(new_project_path)
+      click_button 'Save Project'
+      expect(page).to have_content("8 errors prohibited this project from being saved:")
+    end
+
+    scenario 'if a user only fills out Project Title 7 error messages show' do
+      visit(new_project_path)
+      fill_in 'Project Title', :with => 'Toolshed'
+      click_button 'Save Project'
+      expect(page).to have_content("7 errors prohibited this project from being saved:")
+    end
   end
+
   feature 'edit project' do
     before :each do
       org_user_login
       @organization = Organization.create(name:"Fishy", website_url:"www.google.com", description:"I like saving fish")
     end
+
     scenario 'a user can delete a project' do
       add_project
       click_button 'Delete Project'
       expect(page).to have_selector('li', count:0)
     end
+
     scenario 'a user can click the edit projects button' do
       pending
       add_project
