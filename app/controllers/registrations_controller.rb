266cc3dea919028c142b25f62ab54555f7334f8f
Mark Janzer <email": null>
2016-01-11 14:24:57 -0800

    Fix rspec. Remove users/ from registration route

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,33 +1,33 @@
 class RegistrationsController < Devise::RegistrationsController
 
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        sign_in @user
-        if @user.org_affiliate
-          redirect_to new_organization_path
-        else
-          redirect_to projects_path
-        end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      if @user.org_affiliate
+        redirect_to new_organization_path
       else
-        p @errors = @user.errors.messages
-        redirect_to new_user_registration_path
+        redirect_to projects_path
       end
+    else
+      p @errors = @user.errors.messages
+      redirect_to new_user_registration_path
     end
+  end
 
-    private
+  private
 
-    def user_params
-      user_permitted = %i(
-        first_name
-        last_name
-        email
-        password
-        org_affiliate
-        password_confirmation
-        public_profile_url
-        bootcamp
-      )
-      params.require(:user).permit(user_permitted)
-    end
+  def user_params
+    user_permitted = %i(
+      first_name
+      last_name
+      email
+      password
+      org_affiliate
+      password_confirmation
+      public_profile_url
+      bootcamp
+    )
+    params.require(:user).permit(user_permitted)
+  end
 end
--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -92,7 +92,7 @@ describe 'feature testing', :type => :feature, js: true do
       org_user_login
       new_org
       click_button 'Delete Account'
-      expect(page).to have_content("Welcome To Code-laborate")
+      expect(page).to have_content("Welcome To <code>laborate")
     end
   end
 end
