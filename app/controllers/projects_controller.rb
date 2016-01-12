b6bd18bff568772cda5fbf4b147aaf10c3d37e9e
Mark Janzer <email": null>
2016-01-12 15:15:00 -0800

    Fix strange merge conflict

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -33,8 +33,14 @@ class ProjectsController < ApplicationController
   def update
     @project = Project.find(params[:project][:id])
     @organization = Organization.find(@project.organization_id)
+    @user = current_user
     if @project.update(project_params)
-      render template: "projects/_show_project", :layout => false
+      if @project.developer_id
+        UserMailer.dev_project(@project,@user).deliver_later
+        render template: "projects/_show_project", :layout => false
+      else
+        render template: "projects/_show_project", :layout => false
+      end
     else
       p 'in else, need error'
     end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,11 @@ class UserMailer < ApplicationMailer
     @url  = 'http://localhost:3000/users/sign_up'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
+
+  def dev_project(project, user)
+    @project = project
+    @user = user
+    @url = "http://localhost:3000/projects/#{@project.id}"
+    mail(to: @project.contact_email, subject: "Your project was selected")
+  end
 end
