dac7bb84546c467e72e29b5b2506f3ee9a3c665e
Danielle Cameron <danyell85@gmail.com>
2016-01-12 12:38:08 -0800

    Start project email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,10 @@ class UserMailer < ApplicationMailer
     @url  = 'http://localhost:3000/users/sign_up'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
+
+  def dev_project(project)
+    @product = project
+    @url = ''
+    mail(to: @project.contact_mail, subject: "Your project was selected")
+  end
 end
