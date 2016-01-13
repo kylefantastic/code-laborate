77661848f9185a1a34c0d08306d15316f1140afd
Kyle Smith <kylefantastic@gmail.com>
2016-01-13 10:01:38 -0800

    Make a method to check phone numbers

--- app/models/project.rb
+++ app/models/project.rb
@@ -11,6 +11,19 @@ class Project < ActiveRecord::Base
     where("description like ? or title like ?" , "%#{query}%", "%#{query}%")
   end
 
+  def validatePhoneNumber
+    if /\W*([2-9][0-8][0-9])\W*([2-9][0-9]{2})\W*([0-9]{4})(\se?x?t?(\d*))?/.match(self.contact_phone)
+      p "UA" * 16
+      p self
+      p ":0 " * 12
+      self.save
+    else
+      p "PO" * 16
+      p self.errors
+      p "PO" * 16
+    end
+  end
+
   # def project_notification(record)
   #   @record = record
   #    mail to: "danyell85@gmail.com", subject: "You project got selected."
