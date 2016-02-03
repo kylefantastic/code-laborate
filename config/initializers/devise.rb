901aaab4c4ca3454d459f024a9a1273506babadb
Danielle Cameron <danyell85@gmail.com>
2016-02-03 11:30:08 -0800

    Remove extra / from pathing

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -22,7 +22,7 @@ Devise.setup do |config|
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
   require 'devise/orm/active_record'
-  config.omniauth :facebook,  ENV["FACEBOOK_APP_ID"] , ENV["FACEBOOK_APP_SECRET"], callback_url: "http://code-laborate.herokuapp.com//users/auth/facebook/callback"
+  config.omniauth :facebook,  ENV["FACEBOOK_APP_ID"] , ENV["FACEBOOK_APP_SECRET"], callback_url: "http://code-laborate.herokuapp.com/users/auth/facebook/callback"
   # Configure which keys are used when authenticating a user. The default is
   # just :email. You can configure it to use [:username, :subdomain], so for
   # authenticating a user, both parameters are required. Remember that those
