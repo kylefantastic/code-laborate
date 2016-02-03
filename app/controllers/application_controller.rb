cc309796ee29e6ed57a41f7ae3aeca84e7009deb
Danielle Cameron <danyell85@gmail.com>
2016-02-03 12:58:24 -0800

    Add filter for facebook callback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
+  skip_before_filter :verify_authenticity_token
   # before_action :authenticate_user!
   before_filter :configure_permitted_parameters, if: :devise_controller?
   protected
